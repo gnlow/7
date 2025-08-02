6658e6523bf0eb588827c67663124642ef2c0ca0
Gnlow <gnlow@pm.me>
2025-08-03 08:32:28 +0900

    init

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { html, render } from "https://esm.sh/lit-html@3.3.0"
+import "https://esm.sh/adorable-css@1.6.2"
+
+render(html`
+<div class="p(20) line-height(0.8)">
+    <h1 class="font(40)">7</h1>
+</div>
+`, document.body)
